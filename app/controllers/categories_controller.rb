498783d938756e4b1e33a90aa44c99c50feaf041
Steven <stevenchoy0915@gmail.com>
2021-08-31 19:28:26 +0800

    added different actions

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,33 +1,47 @@
 class CategoriesController < ApplicationController
+  
+  def index
+    @categories = Category.all
+  end
 
   def new
     @category = Category.new
   end
 
+  def show
+    @category = Category.find(params[:id])
+  end
+
   def create
     @category = Category.new(category_params)
-    if @category.save
-      flash[:notice] = "Category was successfully created"
+    if @category.save 
+      flash[:notice] = "Category was successfuly created"
       redirect_to @category
     else
       render 'new'
     end
-
   end
 
-    def index
-      @categories = Category.all
-    end
+  def edit
+    @category = Category.find(params[:id])
+  end
 
-    def show
-      @category = Category.find(params[:id])
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:notice] = "Category was updated successfully"
+      redirect_to @category
+    else
+      render 'edit'
     end
+   end
+
 
 
   private
 
   def category_params
-    params.require(:category).permit(:name)
+    params.require(:category).permit(:category_name, :remarks)
   end
 
 end
