24bb5f71854abc7669ea7166c1f2aba9837bd9fe
Steven <stevenchoy0915@gmail.com>
2021-09-02 16:04:43 +0800

    add create user function

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
- 
+  
   resources :categories do
     resources :agendas
   end
+  
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
   root "categories#index"
 end
