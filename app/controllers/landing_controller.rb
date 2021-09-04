84df7aec78a72e9d83c072042d76c1b86239727d
Steven <stevenchoy0915@gmail.com>
2021-09-04 10:05:22 +0800

    improve UI in categories

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -6,5 +6,10 @@ class LandingController < ApplicationController
 
   def about
   end
+
+  def index
+    @categories = Category.all
+  end
+
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'landing#home'
   get 'about', to: 'landing#about'
+  get 'index', to: 'landing#index'
   resources :categories do
     resources :agendas
   end
